d898b14fd6805e5b454995560a2c9531a01efd00
Enrique Jimenez <ejimenez2292@gmail.com>
2022-07-02 23:34:27 -0500

    added mutations and user auth

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client'
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
 
+  const [addUser, { error }] = useMutation(ADD_USER)
+  
   // update state based on form input changes
   const handleChange = event => {
     const { name, value } = event.target;
@@ -16,7 +21,17 @@ const Signup = () => {
   // submit form
   const handleFormSubmit = async event => {
     event.preventDefault();
-  };
+  
+    try {
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+    
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <main className="flex-row justify-center mb-4">
@@ -56,6 +71,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
